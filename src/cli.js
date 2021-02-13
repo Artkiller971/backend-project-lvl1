d667286450e22cfa98ef3579cda263e01a75b17e
artkiller <koshevnikod@gmail.com>
2021-02-13 20:57:10 +0300

    fixing eslint

--- src/cli.js
+++ src/cli.js
@@ -3,5 +3,7 @@ import readlineSync from 'readline-sync';
 export const userName = () => {
   const name = readlineSync.question('What is you name? ');
   console.log(`Hi, ${name}!`);
-  return;
 };
+
+
+
