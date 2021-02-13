8172853423d5cccfe6ebf457b6352ac55806789c
artkiller <koshevnikod@gmail.com>
2021-02-13 19:14:46 +0300

    userName function

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export const userName = () => {
+  const name = readlineSync.question('What is you name? ');
+  console.log(`Hi, ${name}!`);
+  return;
+};
