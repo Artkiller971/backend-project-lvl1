68f861bcd8dd17982bcf4b6991cbb4bd3bd36080
artkiller <koshevnikod@gmail.com>
2021-02-19 22:37:44 +0300

    added brain-prime game

---
+++ src/games/prime.js
@@ -0,0 +1,27 @@
+import random from '../random.js';
+import gameEngine from '../index.js';
+
+const isPrime = (num) => {
+  if (num < 3) {
+    return false;
+  }
+
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const gameObjective = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getQuestionAndAnswer = () => {
+  const num = random();
+  const question = num;
+  const answer = isPrime(num) ? 'yes' : 'no';
+  return [question, answer];
+};
+
+export default () => gameEngine(gameObjective, getQuestionAndAnswer);
